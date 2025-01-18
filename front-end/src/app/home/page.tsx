a9a70254d2a6909afa5917b5ac60d6c84474cf68
m3di0cre3 <anandtandon8@gmail.com>
2025-01-18 02:00:11 -0500

    created navbar

---
+++ front-end/src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Navbar from "@/app/components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="bg-orange-100">
+      <Navbar />
+    </div>
+  );
+}
