62fa06edd498a24745b1de9c51dec7ffe26b4463
enxilium <jace.zk.dev@gmail.com>
2025-01-18 15:05:45 -0500

    chat/gemini fcuntioanlity

--- front-end/src/app/home/page.tsx
+++ front-end/src/app/home/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Navbar from "@/app/components/Navbar";
+import { Chat } from "../components/Chat";
 
 export default function Home() {
   return (
     <div className="bg-orange-100">
-      <Navbar />
+      <Chat/>
     </div>
   );
 }
