1d5a6d5c8bd9d4d582a1d252c89aeb8d70d4c535
enxilium <jace.zk.dev@gmail.com>
2025-01-19 00:55:52 -0500

    added chat functionality

--- front-end/src/app/home/page.tsx
+++ front-end/src/app/home/page.tsx
@@ -276,6 +276,9 @@ export default function Home() {
         
         <PollingModal isOpen={isPollingOpen} onClose={handleClose} />
       </div>
+      <div className="fixed bottom-4 right-4 z-50">
+        <Chat />
+      </div>
     </div>
   );
 }
