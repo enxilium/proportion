84931676ed44e06e76423ea570b290ab8f8b379f
m3di0cre3 <anandtandon8@gmail.com>
2025-01-31 19:49:18 -0500

    change cookie maxage

--- front-end/src/app/api/setget-name/route.ts
+++ front-end/src/app/api/setget-name/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
     path: '/',
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
-
+    maxAge: 60 * 60 * 24,
     httpOnly: true
   });
 
